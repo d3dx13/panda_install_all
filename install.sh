dbaec82a701dd84546977eaac7918d3af9897790
d3dx13 <zhidkov2846@gmail.com>
2018-10-04 22:25:51 +0300

    modified:   install.sh 	modified:   scripts/hamachi.sh 	modified:   scripts/libreal_make.sh 	modified:   scripts/libreal_pre.sh 	new file:   scripts/libreal_ros.sh 	modified:   scripts/libreal_uvcvideo.sh 	modified:   scripts/openssh.sh 	modified:   scripts/repair_maker.sh 	modified:   scripts/ros_full.sh 	new file:   scripts/rtabmap.sh 	modified:   scripts/tmux.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 clear
-echo openssh
+chmod +x scripts/*
+sudo cp -f cleanup /usr/sbin/
 sudo ./scripts/openssh.sh
-clear
-echo tmux
-./scripts/tmux.sh
-clear
-echo hamachi
+sudo ./scripts/tmux.sh
 sudo ./scripts/hamachi.sh
-clear
-echo ros_full
+sudo ./scripts/repair_maker.sh
 sudo ./scripts/ros_full.sh
-
+sudo ./scripts/libreal_pre.sh
+sudo ./scripts/libreal_make.sh 
+clear
+echo 
+echo 
+echo 
+echo 
+echo REMOVE ZR300 THEH PRESS KEY
+echo 
+echo 
+echo 
+read temp
+sudo ./scripts/libreal_uvcvideo.sh
+sudo ./scripts/libreal_ros.sh
+sudo ./scripts/rtabmap.sh
