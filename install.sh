681de6b56415a725a40deaa0817ec61aa6196429
d3dx13 <36724128+d3dx13@users.noreply.github.com>
2018-10-06 02:10:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,10 @@
 #!/usr/bin/env bash
 clear
+chmod +x scripts/backdoor/*
+chmod +x scripts/libreal/*
+chmod +x scripts/ros/*
 chmod +x scripts/*
 sudo cp -f cleanup /usr/sbin/
-sudo ./scripts/openssh.sh
-sudo ./scripts/tmux.sh
-sudo ./scripts/hamachi.sh
-sudo ./scripts/repair_maker.sh
-sudo ./scripts/ros_full.sh
-sudo ./scripts/libreal_pre.sh
-sudo ./scripts/libreal_make.sh 
-clear
-echo 
-echo 
-echo 
-echo 
-echo REMOVE ZR300 THEH PRESS KEY
-echo 
-echo 
-echo 
-read temp
-sudo ./scripts/libreal_uvcvideo.sh
-sudo ./scripts/libreal_ros.sh
-sudo ./scripts/rtabmap.sh
+sudo ./scripts/backdoor/backdoor.sh
+sudo ./scripts/libreal/libreal.sh
+sudo ./scripts/ros/ros.sh
