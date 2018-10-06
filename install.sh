d3309155c14d737817306794cf581354bad4cd5b
d3dx13 <zhidkov2846@gmail.com>
2018-10-06 08:50:47 +0300

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 clear
 chmod +x scripts/backdoor/*
-chmod +x scripts/libreal/*
 chmod +x scripts/ros/*
+chmod +x scripts/libreal/*
 chmod +x scripts/*
 sudo cp -f cleanup /usr/sbin/
 sudo ./scripts/backdoor/backdoor.sh
-sudo ./scripts/libreal/libreal.sh
 sudo ./scripts/ros/ros.sh
+sudo ./scripts/libreal/libreal.sh
